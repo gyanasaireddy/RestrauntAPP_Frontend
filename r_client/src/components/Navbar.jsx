2f41bb87acb08e72c70f13d5cdd37ee0bee5735a
Gyana Sai Reddy <gjnanasai@student.nitw.ac.in>
2025-07-23 00:27:39 +0530

    Fixed Header as of now

--- r_client/src/components/Navbar.jsx
+++ r_client/src/components/Navbar.jsx
@@ -1,89 +1,58 @@
 import React from "react";
+import '../App.css'
 
 function Navbar() {
   return (
-    <nav
-      class="navbar navbar-expand-lg bg-body-tertiary rounded"
-      aria-label="Thirteenth navbar example"
-    >
-      {" "}
-      <div class="container-fluid">
-        {" "}
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarsExample11"
-          aria-controls="navbarsExample11"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          {" "}
-          <span class="navbar-toggler-icon"></span>{" "}
-        </button>{" "}
-        <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-          {" "}
-          <a class="navbar-brand col-lg-3 me-0" href="#">
-            <img src="/logo.png" alt=""  className="App-logo"/>
-            The Clay Pot Kitchen
-          </a>{" "}
-          <ul class="navbar-nav col-lg-6 justify-content-lg-center">
-            {" "}
-            <li class="nav-item">
-              {" "}
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>{" "}
-            </li>{" "}
-            <li class="nav-item">
-              {" "}
-              <a class="nav-link" href="#">
-                Link
-              </a>{" "}
-            </li>{" "}
-            <li class="nav-item">
-              {" "}
-              <a class="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>{" "}
-            </li>{" "}
-            <li class="nav-item dropdown">
-              {" "}
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>{" "}
-              <ul class="dropdown-menu">
-                {" "}
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>{" "}
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>{" "}
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>{" "}
-              </ul>{" "}
-            </li>{" "}
-          </ul>{" "}
-          <div class="d-lg-flex col-lg-3 justify-content-lg-end">
-            {" "}
-            <button class="btn btn-primary">Button</button>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
-    </nav>
+          <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
+        <div className="container-fluid">
+          <a className="navbar-brand d-flex align-items-center" href="/">
+            <img src="/logo.png" alt="Logo" className="App-logo mx-2" />
+            <span>The Clay Pot Kitchen</span>
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample11"
+            aria-controls="navbarsExample11"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+            <ul className="navbar-nav col-lg-6 justify-content-lg-center">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/menu">Menu</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">Contact</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                  More
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="/reserve">Reserve</a></li>
+                  <li><a className="dropdown-item" href="/admin">Admin Login</a></li>
+                </ul>
+              </li>
+            </ul>
+
+            <div className="d-lg-flex col-lg-3 justify-content-lg-end">
+              <a href="/reserve" className="btn btn-dark">Make Reservation</a>
+            </div>
+          </div>
+        </div>
+      </nav>
   );
 }
 
