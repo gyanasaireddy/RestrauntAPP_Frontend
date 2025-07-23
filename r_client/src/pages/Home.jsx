11c4f11dda8a069165ccf6c143eb4513212f7188
Gyana Sai Reddy <gjnanasai@student.nitw.ac.in>
2025-07-24 01:13:28 +0530

    Updated Fixed Hero Page Navbar and Footer

--- r_client/src/pages/Home.jsx
+++ r_client/src/pages/Home.jsx
@@ -18,15 +18,20 @@ const Home = () => {
         </div>
       </header> */}
 
-      <main role="main" className="inner cover align-self-center mt-5 pt-5">
-        <img src="/logo.png" alt="Logo" className="App-logo mx-2"  />
-        <h1 className="cover-heading">The Clay Pot Kitchen</h1>
-        <p className="lead">
-          Cover is a one-page template for building simple and beautiful home pages.
-          Download, edit the text, and add your own fullscreen background photo to make it your own.
+      <main role="main" className="inner cover align-self-center mt-5 pt-5 px-0">
+        <img src="/logo.png" alt="Logo" className="logo m-2"  />
+        <h1 className="cover-heading display-4 fw-bol">The Clay Pot Kitchen</h1>
+        <p >
+          <strong className='fw-semibold fst-italic mb-3 lead'>Rustic Recipes. Contemporary Charm.</strong>
+          <br />
+          <span className=' fst-italic mb-3 '>
+           The Clay Pot Kitchen reimagines comfort food<br />through 
+slow cooking, bold spices, and artistic presentation.  
+          </span>
+
         </p>
         <p className="lead">
-          <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
+          <a href="#" className="btn btn-lg btn-outline-light fw-bold px-4">Reserve</a>
         </p>
       </main>
 
